b3c57098fdfe7d8ae7851b911633dd336ef2e1a0
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-01-03 23:34:57 -0500

    Changed avatar default size to 150px. Attempting bug fix for assets not loading in production

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,6 +1,6 @@
 module AvatarHelper
   def avatar
-    image_tag current_user.gravatar_url,
+    image_tag current_user.gravatar_url(size: 150),
     alt: "#{current_user.email}'s Avatar"
   end
 end
