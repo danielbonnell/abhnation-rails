0e37b371b9b29ef54e73f71d111b559880e964c7
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-01-04 23:20:35 -0500

    Added validations for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
 end
