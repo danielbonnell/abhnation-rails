ebdafd8df78b56b589514644f01172e0db8644c4
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-01-04 22:06:16 -0500

    Moved helpers to support/helpers. Required helpers in rails_helper.rb

--- spec/features/user_registers_spec.rb
+++ spec/features/user_registers_spec.rb
@@ -1,34 +1,36 @@
 require 'rails_helper'
 
 feature 'user registers', %Q{
-  As a visitor
-  I want to register
-  So that I can create an account
-} do
+  As a user, I want to register an account, so that I can view restricted content.
+  Acceptance Criteria:
+  * [X] - I have an option to register an account
+  * [X] - I must supply a username, email, and password.
+  * [X] - I am redirected to my account overview upon successful registration.
+  * [ ] - I must confirm my email address.
+  * [ ] - If I don’t confirm my email address within 24 hours, my account will be deleted.
+  * [X] - I see an error message if my registration credentials are invalid.
+  } do
 
-  # Acceptance Criteria:
-  # * I must specify a valid email address,
-  #   password, and password confirmation
-  # * If I don't specify the required information, I am presented with
-  #   an error message
+
+  username = "Lafiel_Abriel"
+  email = "lafiel.abriel@abhnation.com"
 
   scenario 'provide valid registration information' do
     visit new_user_registration_path
 
-    fill_in 'Email', with: 'john@example.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
-
-    click_button 'Log up'
+    register_user(username, email)
 
     expect(page).to have_content('Welcome! You have signed up successfully.')
+    expect(page).to have_content(username)
+    expect(page).to have_content(email)
     expect(page).to have_content('Log Out')
   end
 
   scenario 'provide invalid registration information' do
     visit new_user_registration_path
 
-    click_button 'Log up'
+    click_button 'Log in'
+
     expect(page).to have_content("can't be blank")
     expect(page).to_not have_content('Log Out')
   end
