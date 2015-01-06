7bee976064b6d344e466d45c286e5e330b91999a
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-01-05 20:59:16 -0500

    Tests for editing user profile and email passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
+  validates :age, :allow_blank => true,
+    numericality: { only_integer: true },
+    inclusion: { in: 0..300 }
+  validates :website, :allow_blank => true,
+    format: { with: URI.regexp },
+    if: Proc.new { |a| a.website.present? }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105032931) do
+ActiveRecord::Schema.define(version: 20150106013315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(version: 20150105032931) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username",                            null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "age"
+    t.string   "website"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
