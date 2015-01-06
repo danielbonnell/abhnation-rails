601fd126a351d7049fa116ad3bd0374e84d1d189
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-01-05 20:19:06 -0500

    Changed 'Sign in' to 'Log in'

--- spec/support/helpers/authentication.rb
+++ spec/support/helpers/authentication.rb
@@ -1,13 +1,12 @@
 module Helpers
   module Authentication
-    def sign_in_as(user)
+    def log_in_as(user)
       visit new_user_session_path
 
       within "#new_user" do
-        fill_in "Username", with: user.username
-        fill_in "Email", with: user.email
-        fill_in "Password", with: user.password
-        click_on "Sign in"
+        fill_in 'user[email]', with: user.email
+        fill_in 'user[password]', with: user.password
+        click_on "Log in"
       end
     end
   end
