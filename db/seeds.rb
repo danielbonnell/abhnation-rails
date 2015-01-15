917e0b1334354a869ea5f36d9660daa610595204
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-01-15 12:32:32 -0500

    Use faker gem to generate seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,38 @@
-User.create!(
-  email: "acidstealth@gmail.com",
-  username: "ACIDSTEALTH",
-  password: "password",
-  password_confirmation: "password"
-)
+3.times do
+  password = Faker::Internet.password
+  User.create!(
+    email: Faker::Internet.email,
+    username: Faker::Internet.user_name,
+    password: password,
+    password_confirmation: password
+  )
 
-Category.create!(
-  name: "Fake Category",
-  user_id: User.find_by(username: "ACIDSTEALTH").id
-)
+  Category.create!(
+    name: Faker::Lorem.characters(10),
+    user_id: User.last.id
+  )
 
-Category.create!(
-  name: "Fake Category 2",
-  user_id: User.find_by(username: "ACIDSTEALTH").id
-)
+  Article.create!(
+    title: Faker::Lorem.characters(10),
+    slug: Faker::Lorem.characters(10),
+    text: Faker::Lorem.words(100),
+    category_id: Category.last.id,
+    user_id: User.last.id
+  )
+end
 
-Category.create!(
-  name: "Fake Subcat.",
-  parent_id: Category.find_by(name: "Fake Category").id,
-  user_id: User.find_by(username: "ACIDSTEALTH").id
-)
+10.times do
+  Category.create!(
+    name: Faker::Lorem.characters(10),
+    parent_id: Category.last.id,
+    user_id: User.last.id
+  )
 
-Article.create!(
-  title: "Example Article",
-  slug: "Example Article",
-  text: Faker::Lorem.words(100).join(' '),
-  category_id: Category.find_by(name: "Fake Subcat.").id,
-  user_id: User.find_by(username: "ACIDSTEALTH").id
-)
-
-Article.create!(
-  title: "Fake Article 2",
-  slug: "Fake Article 2",
-  text: Faker::Lorem.words(100).join(' '),
-  category_id: Category.find_by(name: "Fake Category 2").id,
-  user_id: User.find_by(username: "ACIDSTEALTH").id
-)
+  Article.create!(
+    title: Faker::Lorem.characters(10),
+    slug: Faker::Lorem.characters(10),
+    text: Faker::Lorem.words(100),
+    category_id: Category.last.id,
+    user_id: User.last.id
+  )
+end
