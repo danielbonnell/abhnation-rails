76b1c6017adf6407d62a3bade710c8a509448f49
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-01-16 13:58:39 -0500

    Fixed @font-face bug causing wbe fonts not to render in Firefox. Added breadcrumbs to user profile views

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -23,6 +23,32 @@ crumb :article do |article|
   parent :category
 end
 
+crumb :users do
+  link "Users", users_path
+  parent :root
+end
+
+crumb :user do |user|
+  link user.username, user
+  parent :users
+end
+
+crumb :edit_user do |edit_user|
+  link edit_user.username, edit_user
+  link "Edit Profile", edit_user
+  parent :users
+end
+
+crumb :log_in do |log_in|
+  link "Log in", log_in
+  parent :root
+end
+
+crumb :registration do |registration|
+  link "Register", registration
+  parent :root
+end
+
 # If you want to split your breadcrumbs configuration over multiple files, you
 # can create a folder named `config/breadcrumbs` and put your configuration
 # files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :users, only: [:show, :edit, :put]
+  resources :users, only: [:show, :edit, :put, :index]
 
   resources :categories, only: [:show, :index]
   resources :articles, only: [:show, :index]
