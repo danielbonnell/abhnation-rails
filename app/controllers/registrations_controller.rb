fb07b8b161ffd549217243f1c9676db0de01c227
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-01-18 19:47:08 -0500

    Refactored code based on Hound CI

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,22 +3,32 @@ class RegistrationsController < Devise::RegistrationsController
     update_params = account_update_params
     # required for settings form to submit when password is left blank
     if update_params[:password].blank?
-      [:password,:password_confirmation,:current_password].collect{|p| update_params.delete(p) }
-    end
-    if update_params[:password].present? && update_params[:current_password].blank?
-      [:current_password].collect{|p| update_params.delete(p) }
+      [:password,:password_confirmation,:current_password].collect do
+        |p| update_params.delete(p)
+      end
     end
 
-    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+    if update_params[:password].present? && update_params[:current_password].blank?
+      [:current_password].collect{ |p| update_params.delete(p) }
+    end
+
+    self.resource = resource_class.to_adapter.get!(
+      send(:"current_#{resource_name}").to_key
+    )
+
+    if resource.respond_to?(:unconfirmed_email)
+      prev_unconfirmed_email = resource.unconfirmed_email
+    end
 
     if update_resource(resource, update_params)
       yield resource if block_given?
+      
       if is_flashing_format?
         flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
           :update_needs_confirmation : :updated
         set_flash_message :notice, flash_key
       end
+
       sign_in resource_name, resource, bypass: true
       respond_with resource, location: after_update_path_for(resource)
     else
