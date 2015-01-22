a426e9a6eb38fe65f03b8855064887452202c27d
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-01-22 15:48:51 -0500

    Passing tests for moving parent categories up and down 1 or 2 times using model method reorder

--- app/models/category.rb
+++ app/models/category.rb
@@ -22,6 +22,49 @@ class Category < ActiveRecord::Base
     presence: true,
     numericality: { only_integer: true }
 
+  def reorder(index_param)
+    if self.parent.nil?
+      if index_param == -1
+        swap_cat = Category.cat_parents.where("display_index < ?", self.display_index).last
+        swap_cat_index = swap_cat.display_index
+        cat_index = self.display_index
+        self.update(display_index: swap_cat_index)
+        swap_cat.update(display_index: cat_index) unless swap_cat_index.nil?
+      else
+        swap_cat = Category.cat_parents.where("display_index > ?", self.display_index).first
+        swap_cat_index = swap_cat.display_index
+        cat_index = self.display_index
+        # binding.pry
+        self.update(display_index: self.display_index + 1)
+        swap_cat.update(display_index: swap_cat_index - 1) unless swap_cat_index.nil?
+      end
+    else
+      if index_param == -1
+
+      else
+
+      end
+    end
+
+    # if self.parent.nil? && index_param == 1
+    #   prime_cat_index = self.display_index + 1
+    #   swap_cat = Category.cat_parents.where("display_index <= ?", prime_cat_index + 1).first
+    #   swap_cat_index = swap_cat.display_index - 1
+    # elsif self.parent.nil? && index_param == -1
+    #   prime_cat_index = self.display_index - 1
+    #   swap_cat = Category.cat_parents.where("display_index >= ?", prime_cat_index - 1).first
+    #   swap_cat_index = swap_cat.display_index + 1
+    # elsif !self.parent.nil? && index_param == 1
+    #   prime_cat_index = self.display_index + 1
+    #   swap_cat = Category.where("parent_id is not NULL and display_index <= ?", prime_cat_index + 1).first
+    #   swap_cat_index = swap_cat.display_index - 1
+    # elsif !self.parent.nil? && index_param == -1
+    #   prime_cat_index = self.display_index - 1
+    #   swap_cat = Category.where("parent_id is not NULL and display_index >= ?", prime_cat_index - 1).first
+    #   swap_cat_index = swap_cat.display_index + 1
+    # end
+  end
+
   def self.cat_names_dropdown
     cat_hash = {}
     self.all.each do |category|
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,18 +3,50 @@ require 'rails_helper'
 RSpec.describe Category, :type => :model do
 
 
-  context 'moving category up' do
+  context 'moving parent category up or down once: ' do
 
-    it 'display_index decreases by 1', focus: true do
-      category = FactoryGirl.create(:category, display_index: 2)
-      swap_category = FactoryGirl.create(:category, display_index: 1)
+    it 'UPx1: display_index decreases by 1 for category and increased by 1 for swap', focus: true do
+      FactoryGirl.create(:category, name: "swap_cat", display_index: 1)
+      category = FactoryGirl.create(:category, name: "cat", display_index: 2)
 
-      category.up_index
-      expect(category.display_index).to eq(1)
+      category.reorder(-1)
+      expect(Category.find_by(name: "cat").display_index).to eq(1)
+      expect(Category.find_by(name: "swap_cat").display_index).to eq(2)
     end
 
-    # it 'display_index of swap category increases by 1' do
-    #
-    # end
+    it 'DOWNx1: display_index increases by 1 for category and decreases by 1 for swap', focus: true do
+      category = FactoryGirl.create(:category, name: "cat", display_index: 1)
+      FactoryGirl.create(:category, name: "swap_cat", display_index: 2)
+
+      category.reorder(1)
+      expect(Category.find_by(name: "cat").display_index).to eq(2)
+      expect(Category.find_by(name: "swap_cat").display_index).to eq(1)
+    end
+  end
+
+  context 'moving parent category up or down twice: ' do
+
+    it 'UPx2: display_index decreases by 2 for category and increased by 2 for swap', focus: true do
+      FactoryGirl.create(:category, display_index: 0)
+      FactoryGirl.create(:category, name: "swap_cat", display_index: 1)
+      category = FactoryGirl.create(:category, name: "cat", display_index: 2)
+
+      category.reorder(-1)
+      category.reorder(-1)
+      expect(Category.find_by(name: "cat").display_index).to eq(0)
+      expect(Category.find_by(name: "swap_cat").display_index).to eq(2)
+    end
+
+    it 'DOWNx2: display_index increases by 2 for category and decreases by 2 for swap', focus: true do
+      first = FactoryGirl.create(:category, name: "first_cat", display_index: 0)
+      second = FactoryGirl.create(:category, name: "second_cat", display_index: 1)
+      third = FactoryGirl.create(:category, name: "third_cat", display_index: 2)
+
+      first.reorder(1)
+      first.reorder(1)
+      expect(Category.find_by(name: "first_cat").display_index).to eq(2)
+      expect(Category.find_by(name: "second_cat").display_index).to eq(0)
+      expect(Category.find_by(name: "third_cat").display_index).to eq(1)
+    end
   end
 end
