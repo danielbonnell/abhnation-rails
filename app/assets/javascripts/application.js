5e152505a89914099ee7c4d7d95221ac4b749e6e
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-01-25 23:58:56 -0500

    Attempting fix of bug that causes best_in_place to not work in production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,8 @@
 //= require jquery_ujs
 //= require foundation
 //= require_tree
-//= require best_in_place
 //= require jquery-ui
+//= require best_in_place
 //= require best_in_place.jquery-ui
 
 $(function(){ $(document).foundation(); });
