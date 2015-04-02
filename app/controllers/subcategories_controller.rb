a12bbca579adea037843233109396ffd692f40d7
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-04-02 13:51:57 -0400

    Subcategories model no longer exists

--- app/controllers/subcategories_controller.rb
+++
@@ -1,9 +0,0 @@
-class SubcategoriesController < ApplicationController
-  def show
-    @subcategory = Subcategory.find(params[:id])
-  end
-
-  def index
-    @subcategory = Subcategory.all
-  end
-end
