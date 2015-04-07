af1f71d15252e711b64032bd87ee3f387dae08ba
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-04-07 19:50:39 -0400

    New controller specs

---
+++ spec/controllers/users_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe 'GET #show' do
+    let(:user) do
+      FactoryGirl.create(:user)
+    end
+
+    before(:each) do
+      get :show, id: user
+    end
+
+    it "Is successful" do
+      expect(response).to be_success
+    end
+
+    it "Returns renders template #show" do
+      expect(response).to render_template("show")
+    end
+
+    it "Returns valid user" do
+      expect(User.last).to eq(user)
+    end
+  end
+end
